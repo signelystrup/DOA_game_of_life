3b1d9e52bac7586d2838bf40040e8b9105b5bd60
signelystrup <166133407+signelystrup@users.noreply.github.com>
2025-11-10 13:05:39 +0100

    calculate dx + dy

--- demo/src/main/java/com/example/demo/entities/Animal.java
+++ demo/src/main/java/com/example/demo/entities/Animal.java
@@ -1,13 +1,30 @@
 package com.example.demo.entities;
 
+import java.util.Random;
+
 public class Animal {
     protected int x, y;
     protected int gridX, gridY;
+    protected double dx, dy;
+    protected int SPEED = 6;
 
     public Animal(int x, int y){
         this.x = x;
         this.y = y;
         gridX = 0; //TODO: calculate.
         gridY = 0;
+
+        Random random = new Random();
+        this.dx = calculateDx(random.nextDouble());
+        this.dy = calculateDy(this.dx);
     }
+
+    public double calculateDx(double dy){
+        return SPEED/dy;
+    }
+
+    public double calculateDy(double dx){
+        return SPEED/dx;
+    }
+
 }
--- demo/src/main/java/com/example/demo/entities/Bunny.java
+++ demo/src/main/java/com/example/demo/entities/Bunny.java
@@ -1,9 +1,17 @@
 package com.example.demo.entities;
 
+import lombok.Getter;
+
+import java.util.Random;
+
+@Getter
 public class Bunny extends Animal {
     static final int SPEED = 5;
 
     public Bunny(int x, int y){
         super(x, y);
+
     }
+
+
 }
